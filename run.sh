493f2882845e5b09c0dcff582233b6af581982ff
Alexander De Leon <me@alexdeleon.name>
2015-04-30 09:34:01 +0200

    It is necessary to remove and copy files to fix file permission

--- run.sh
+++ run.sh
@@ -1,4 +1,3 @@
 #!/bin/sh
-chown -R postgres /etc/ssl/private
-/etc/init.d/postgresql start
+mkdir /etc/ssl/private-copy; mv /etc/ssl/private/* /etc/ssl/private-copy/; rm -r /etc/ssl/private; mv /etc/ssl/private-copy /etc/ssl/private; chmod -R 0700 /etc/ssl/private; chown -R postgres /etc/ssl/private && /etc/init.d/postgresql start
 catalina.sh run
