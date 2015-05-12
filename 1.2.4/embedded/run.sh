2dfaef401f5c0d45974495c172bc38c535e2f1da
Alexander De Leon <me@alexdeleon.name>
2015-05-12 15:55:45 +0200

    Added Solr

--- 1.2.4/embedded/run.sh
+++ 1.2.4/embedded/run.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
 mkdir /etc/ssl/private-copy; mv /etc/ssl/private/* /etc/ssl/private-copy/; rm -r /etc/ssl/private; mv /etc/ssl/private-copy /etc/ssl/private; chmod -R 0700 /etc/ssl/private; chown -R postgres /etc/ssl/private && /etc/init.d/postgresql start
+cd /opt/transmart-data-1.2.4 && . ./vars && make -C solr start &
 catalina.sh run
