64990f084b34a4bb418ff3b2d024a6fb1ba5ce31
Jose Cruz-Toledo <josemiguelcruztoledo@gmail.com>
2015-06-20 15:14:05 -0700

    added R dependencies

---
+++ 1.2.4/scripts/install_r_packages.R
@@ -0,0 +1,21 @@
+# ipak function: install and load multiple R packages.
+# check to see if packages are installed. Install them if they are not, then load them into the R session.
+
+ipak <- function(pkg){
+  new.pkg <- pkg[!(pkg %in% installed.packages()[, "Package"])]
+  if (length(new.pkg)) 
+    install.packages(new.pkg, dependencies = TRUE)
+  sapply(pkg, require, character.only = TRUE)
+}
+
+
+packages <- c("Rserve", "Cairo", "MASS", "stringr", "ggplot2", "plyr", "reshape2", "gplots", "data.table")
+ipak(packages)
+
+source("biocLite.R")
+biocLite("impute")
+install.packages("WGCNA")
+biocLite("multtest")
+biocLite("limma")
+library(Rserve)
+Rserve()
